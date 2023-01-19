8e56b081c60c9a81d924d2bb3b7e0671737ebf99
Andrii <lutsiukandrey1@gmail.com>
2023-01-19 12:41:20 +0200

    step 4

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,8 @@
+export const Container = ({children}) =>{
+  return(
+      <div className='counter'> 
+         <h1>Please leave feedback</h1>
+       {children}</div> 
+
+      ) 
+   }
---
+++ src/components/Statictics/Statictics.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Notification } from "Notification/Notification";
+export class Statistics extends Component{
+    render(){
+        if(!this.props.countTotalFeedback){
+            return (<Notification message="There is no feedback"/>)
+        }
+        return(
+            
+            <ul className='statistics__conainer'>
+<h2> Statistics</h2>
+        
+             <li>Good : <span className='statistic__value'>{this.props.good}</span> </li>
+            <li>Neutral : <span className='statistic__value'>{this.props.neutral}</span> </li>
+              <li>Bad : <span className='statistic__value'>{this.props.bad}</span> </li>
+              <li>Total : <span className='statistic__value'>{this.props.countTotalFeedback}</span> </li>
+              <li>Positive feedback: <span className='statistic__value'>{this.props.countPositiveFeedbackPercentage}%</span> </li>
+        
+          </ul> 
+        )
+    }
+}
