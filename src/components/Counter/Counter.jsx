84f486ac85acfef76be5992d86c1b3b4dabe20a6
Andrii <lutsiukandrey1@gmail.com>
2023-01-19 13:27:18 +0200

    add styles

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-
+import s from './Counter.module.css'
 export class Counter extends Component{
 render(){
     return(              
         <div className='controls'>
 
-        <button type='button' onClick={this.props.good} >Good</button>
-        <button type='button' onClick={this.props.neutral} >Neutral</button>
-        <button type='button' onClick={this.props.bad} >Bad</button>
+        <button className={s.button} type='button' onClick={this.props.good} >Good</button>
+        <button className={s.button} type='button' onClick={this.props.neutral} >Neutral</button>
+        <button className={s.button} type='button' onClick={this.props.bad} >Bad</button>
     </div>)
 }
 }
