ef19979941b42f26ac4a166f61d5fdb48bb31cde
Andrii <lutsiukandrey1@gmail.com>
2023-03-06 18:15:39 +0200

    2

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,27 @@
-import React, { Component } from "react";
-import s from './Counter.module.css'
-export class Counter extends Component{
-render(){
-    return(              
-        <div className='controls'>
+import React, { Component } from 'react';
+import s from './Counter.module.css';
+import PropTypes from 'prop-types';
 
-        <button className={s.button} type='button' onClick={this.props.good} >Good</button>
-        <button className={s.button} type='button' onClick={this.props.neutral} >Neutral</button>
-        <button className={s.button} type='button' onClick={this.props.bad} >Bad</button>
-    </div>)
+export class Counter extends Component {
+  render() {
+    return (
+      <div className="controls">
+        <button className={s.button} type="button" onClick={this.props.good}>
+          Good
+        </button>
+        <button className={s.button} type="button" onClick={this.props.neutral}>
+          Neutral
+        </button>
+        <button className={s.button} type="button" onClick={this.props.bad}>
+          Bad
+        </button>
+      </div>
+    );
+  }
 }
-}
+
+Counter.propTypes = {
+  good: PropTypes.func,
+  neutral: PropTypes.func,
+  bad: PropTypes.func,
+};
